7ef23c8db6bbdf605b920218d3679fdb8dc44322
George Darchiashvili <gdar91@live.com>
2021-10-23 00:22:31 +0400

    fixed connectable observable return types.

--- Fills.Observable/FillsObservable.MulticastResetting.cs
+++ Fills.Observable/FillsObservable.MulticastResetting.cs
@@ -6,7 +6,7 @@ namespace Fills;
 
 public static partial class FillsObservableExtensions
 {
-    public static IObservable<TElement> MulticastResetting<TElement>(
+    public static IConnectableObservable<TElement> MulticastResetting<TElement>(
         this IObservable<TElement> observable,
         Func<ISubject<TElement>> subjectFactory
     )
@@ -17,14 +17,14 @@ public static partial class FillsObservableExtensions
 
 
 
-    public static IObservable<TElement> PublishResetting<TElement>(
+    public static IConnectableObservable<TElement> PublishResetting<TElement>(
         this IObservable<TElement> observable
     )
     {
         return observable.MulticastResetting(() => new Subject<TElement>());
     }
 
-    public static IObservable<TElement> PublishResetting<TElement>(
+    public static IConnectableObservable<TElement> PublishResetting<TElement>(
         this IObservable<TElement> observable,
         TElement initialValue
     )
@@ -35,14 +35,14 @@ public static partial class FillsObservableExtensions
 
 
 
-    public static IObservable<TElement> ReplayResetting<TElement>(
+    public static IConnectableObservable<TElement> ReplayResetting<TElement>(
         this IObservable<TElement> observable
     )
     {
         return observable.MulticastResetting(() => new ReplaySubject<TElement>());
     }
 
-    public static IObservable<TElement> ReplayResetting<TElement>(
+    public static IConnectableObservable<TElement> ReplayResetting<TElement>(
         this IObservable<TElement> observable,
         int bufferSize
     )
@@ -50,7 +50,7 @@ public static partial class FillsObservableExtensions
         return observable.MulticastResetting(() => new ReplaySubject<TElement>(bufferSize));
     }
 
-    public static IObservable<TElement> ReplayResetting<TElement>(
+    public static IConnectableObservable<TElement> ReplayResetting<TElement>(
         this IObservable<TElement> observable,
         IScheduler scheduler
     )
@@ -58,7 +58,7 @@ public static partial class FillsObservableExtensions
         return observable.MulticastResetting(() => new ReplaySubject<TElement>(scheduler));
     }
 
-    public static IObservable<TElement> ReplayResetting<TElement>(
+    public static IConnectableObservable<TElement> ReplayResetting<TElement>(
         this IObservable<TElement> observable,
         TimeSpan window
     )
@@ -66,7 +66,7 @@ public static partial class FillsObservableExtensions
         return observable.MulticastResetting(() => new ReplaySubject<TElement>(window));
     }
 
-    public static IObservable<TElement> ReplayResetting<TElement>(
+    public static IConnectableObservable<TElement> ReplayResetting<TElement>(
         this IObservable<TElement> observable,
         int bufferSize,
         IScheduler scheduler
@@ -75,7 +75,7 @@ public static partial class FillsObservableExtensions
         return observable.MulticastResetting(() => new ReplaySubject<TElement>(bufferSize, scheduler));
     }
 
-    public static IObservable<TElement> ReplayResetting<TElement>(
+    public static IConnectableObservable<TElement> ReplayResetting<TElement>(
         this IObservable<TElement> observable,
         int bufferSize,
         TimeSpan window
@@ -84,7 +84,7 @@ public static partial class FillsObservableExtensions
         return observable.MulticastResetting(() => new ReplaySubject<TElement>(bufferSize, window));
     }
 
-    public static IObservable<TElement> ReplayResetting<TElement>(
+    public static IConnectableObservable<TElement> ReplayResetting<TElement>(
         this IObservable<TElement> observable,
         TimeSpan window,
         IScheduler scheduler
@@ -93,7 +93,7 @@ public static partial class FillsObservableExtensions
         return observable.MulticastResetting(() => new ReplaySubject<TElement>(window, scheduler));
     }
 
-    public static IObservable<TElement> ReplayResetting<TElement>(
+    public static IConnectableObservable<TElement> ReplayResetting<TElement>(
         this IObservable<TElement> observable,
         int bufferSize,
         TimeSpan window,
