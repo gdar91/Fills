c1fae8b1fa6a77be5b9ac04fb22cf7edf36636bd
George Darchiashvili <gdar91@live.com>
2021-11-06 01:58:58 +0400

    fixed sharing.

---
+++ Fills.Observable/FillsObservable.Share.cs
@@ -0,0 +1,107 @@
+﻿using System.Reactive.Concurrency;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+
+namespace Fills;
+
+public static partial class FillsObservableExtensions
+{
+    private static IObservable<TElement> ShareCore<TElement>(
+        this IObservable<TElement> observable,
+        Func<ISubject<TElement>> subjectFactory
+    )
+    {
+        return observable
+            .Multicast(new ResettingSubject<TElement>(subjectFactory))
+            .RefCount(1, TimeSpan.Zero, ImmediateScheduler.Instance);
+    }
+
+
+
+
+    public static IObservable<TElement> Share<TElement>(
+        this IObservable<TElement> observable
+    )
+    {
+        return observable.ShareCore(() => new Subject<TElement>());
+    }
+
+    public static IObservable<TElement> Share<TElement>(
+        this IObservable<TElement> observable,
+        TElement initialValue
+    )
+    {
+        return observable.ShareCore(() => new BehaviorSubject<TElement>(initialValue));
+    }
+
+
+
+
+    public static IObservable<TElement> ShareReplay<TElement>(
+        this IObservable<TElement> observable
+    )
+    {
+        return observable.ShareCore(() => new ReplaySubject<TElement>());
+    }
+
+    public static IObservable<TElement> ShareReplay<TElement>(
+        this IObservable<TElement> observable,
+        int bufferSize
+    )
+    {
+        return observable.ShareCore(() => new ReplaySubject<TElement>(bufferSize));
+    }
+
+    public static IObservable<TElement> ShareReplay<TElement>(
+        this IObservable<TElement> observable,
+        IScheduler scheduler
+    )
+    {
+        return observable.ShareCore(() => new ReplaySubject<TElement>(scheduler));
+    }
+
+    public static IObservable<TElement> ShareReplay<TElement>(
+        this IObservable<TElement> observable,
+        TimeSpan window
+    )
+    {
+        return observable.ShareCore(() => new ReplaySubject<TElement>(window));
+    }
+
+    public static IObservable<TElement> ShareReplay<TElement>(
+        this IObservable<TElement> observable,
+        int bufferSize,
+        IScheduler scheduler
+    )
+    {
+        return observable.ShareCore(() => new ReplaySubject<TElement>(bufferSize, scheduler));
+    }
+
+    public static IObservable<TElement> ShareReplay<TElement>(
+        this IObservable<TElement> observable,
+        int bufferSize,
+        TimeSpan window
+    )
+    {
+        return observable.ShareCore(() => new ReplaySubject<TElement>(bufferSize, window));
+    }
+
+    public static IObservable<TElement> ShareReplay<TElement>(
+        this IObservable<TElement> observable,
+        TimeSpan window,
+        IScheduler scheduler
+    )
+    {
+        return observable.ShareCore(() => new ReplaySubject<TElement>(window, scheduler));
+    }
+
+    public static IObservable<TElement> ShareReplay<TElement>(
+        this IObservable<TElement> observable,
+        int bufferSize,
+        TimeSpan window,
+        IScheduler scheduler
+    )
+    {
+        return observable.ShareCore(() => new ReplaySubject<TElement>(bufferSize, window, scheduler));
+    }
+}
--- Fills.Observable/ResettingSubject.cs
+++ Fills.Observable/ResettingSubject.cs
@@ -1,10 +1,8 @@
-using System.Reactive.Disposables;
-using System.Reactive.Linq;
 using System.Reactive.Subjects;
 
 namespace Fills;
 
-internal sealed class ResettingSubject<T> : ISubject<T>
+public sealed class ResettingSubject<T> : ISubject<T>
 {
     private readonly Func<ISubject<T>> subjectFactory;
 
@@ -42,90 +40,3 @@ internal sealed class ResettingSubject<T> : ISubject<T>
         return currentSubject.Subscribe(observer);
     }
 }
-
-
-#pragma warning disable CS8632 // The annotation for nullable reference types should only be used in code within a '#nullable' annotations context.
-internal sealed class ConnectableObservable<TSource, TResult> : IConnectableObservable<TResult>
-{
-    private readonly Func<ISubject<TSource, TResult>> _subjectFactory;
-    private readonly BehaviorSubject<ISubject<TSource, TResult>> _subjects;
-    private readonly IObservable<TResult> _results;
-    private readonly IObservable<TSource> _source;
-    private readonly object _gate;
-
-    private Connection? _connection;
-
-    public ConnectableObservable(
-        IObservable<TSource> source,
-        Func<ISubject<TSource, TResult>> subjectFactory
-    )
-    {
-        _subjectFactory = subjectFactory;
-        _subjects = new BehaviorSubject<ISubject<TSource, TResult>>(_subjectFactory());
-        _results = _subjects.Switch();
-        _source = source.AsObservable();
-        _gate = new object();
-    }
-
-    public IDisposable Connect()
-    {
-        lock (_gate)
-        {
-            if (_connection == null)
-            {
-                var subscription = _source.SubscribeSafe(_subjects.Value);
-                var disposable = Disposable.Create(() =>
-                {
-                    lock (_gate)
-                    {
-                        _subjects.OnNext(_subjectFactory());
-                        subscription.Dispose();
-                    }
-                });
-                _connection = new Connection(this, disposable);
-            }
-
-            return _connection;
-        }
-    }
-
-    private sealed class Connection : IDisposable
-    {
-        private readonly ConnectableObservable<TSource, TResult> _parent;
-        private IDisposable? _subscription;
-
-        public Connection(
-            ConnectableObservable<TSource, TResult> parent,
-            IDisposable subscription
-        )
-        {
-            _parent = parent;
-            _subscription = subscription;
-        }
-
-        public void Dispose()
-        {
-            lock (_parent._gate)
-            {
-                if (_subscription != null)
-                {
-                    _subscription.Dispose();
-                    _subscription = null;
-
-                    _parent._connection = null;
-                }
-            }
-        }
-    }
-
-    public IDisposable Subscribe(IObserver<TResult> observer)
-    {
-        if (observer == null)
-        {
-            throw new ArgumentNullException(nameof(observer));
-        }
-
-        return _results.SubscribeSafe(observer);
-    }
-}
-#pragma warning restore CS8632 // The annotation for nullable reference types should only be used in code within a '#nullable' annotations context.
